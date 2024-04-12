87bb6867a11ed30a3108e6f61e2fa056453fee7a
= <=>
2024-04-11 20:38:26 -0500

    Adding courusel component

---
+++ src/routes/home/Corousel.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import one from '../../assets/uno.jpg'
+import two from '../../assets/dos.jpg'
+import three from '../../assets/tres.jpg'
+
+const items = [
+  {
+    src: one,
+    altText: 'wine',
+    caption: 'wine-slider',
+    key: 1,
+  },
+  {
+    src: two,
+    altText: 'wine',
+    caption: 'wine-slider',
+    key: 2,
+  },
+  {
+    src: three,
+    altText: 'wine',
+    caption: 'wine-slider',
+    key: 3,
+  },
+];
+
+function Slider(args) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} width='100%' />
+        <CarouselCaption
+          captionText={item.caption}
+          captionHeader={item.caption}
+        />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+      {...args}
+    >
+      <CarouselIndicators
+        items={items}
+        activeIndex={activeIndex}
+        onClickHandler={goToIndex}
+      />
+      {slides}
+      <CarouselControl
+        direction="prev"
+        directionText="Previous"
+        onClickHandler={previous}
+      />
+      <CarouselControl
+        direction="next"
+        directionText="Next"
+        onClickHandler={next}
+      />
+    </Carousel>
+  );
+}
+
+export default Slider;
--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -5,37 +5,44 @@ import logoWhite from '../../assets/logo_white.png'
 import wines from '../../assets/wines.jpg'
 import bottles from '../../assets/botellas.png'
 
+import Slider from './Corousel'
+
 export const Home = () => {
     return (
         <>
             <div className='section-home'>
                 <div className='logo-home'>
-                    <img className='home-logo' src={ logoWhite } alt="logo" />
+                    <img className='home-logo' src={logoWhite} alt="logo" />
                 </div>
             </div>
             <div className='wines-home'>
                 <div className='image-wine'>
-                    <img className='wine' src={ wines } alt="wines" />
+                    <img className='wine' src={wines} alt="wines" />
                 </div>
                 <div className='description-wine'>
                     <h1 className='title-wine'>
                         Vinos de la Ribera del Duero 100% Ecológicos
                     </h1>
                     <p className='contain-wine'>
-                        Para hacer vino excelente, el tiempo es oro. 
-                        Aunque la historia de Martín Berdugo se remonta 
-                        siglos atrás, el viñedo propio que hoy rodea nuestra 
-                        bodega moderna fue plantado por su dueña, Josefina, 
-                        en 1990 con la asistencia y el apoyo de su esposo 
-                        Enrique, así como de sus tres hijos Antonio, Bruno y 
-                        Pepa. El alma de nuestros vinos se encuentra dentro 
+                        Para hacer vino excelente, el tiempo es oro.
+                        Aunque la historia de Martín Berdugo se remonta
+                        siglos atrás, el viñedo propio que hoy rodea nuestra
+                        bodega moderna fue plantado por su dueña, Josefina,
+                        en 1990 con la asistencia y el apoyo de su esposo
+                        Enrique, así como de sus tres hijos Antonio, Bruno y
+                        Pepa. El alma de nuestros vinos se encuentra dentro
                         de nuestro viñedo arraigado en Ribera del Duero.
                     </p>
                 </div>
             </div>
             <div className='title-products'>
                 <h2 className='some-products'>Algunos de nuestros productos</h2>
-                <img className='bottle-image' src={ bottles } alt="bottles" />
+                <img className='bottle-image' src={bottles} alt="bottles" />
+            </div>
+            <div className='container-slider'>
+                <div className='slider-wrap'>
+                    <Slider />
+                </div>
             </div>
         </>
     )
